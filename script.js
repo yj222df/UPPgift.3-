c86b49b8a5848420f80db88cacc64518f5486317
Yasir Jamal <yj222df@student.lnu.se>
2026-09-25 22:37:26 +0200

    Add files via upload
    Yasir Jamal

---
+++ script.js
@@ -0,0 +1,6 @@
+const knapp = document.getElementById("knapp");
+const rubrik = document.getElementById("rubrik");
+
+knapp.addEventListener("click", function() {
+    rubrik.textContent = "Goodbye World.....";
+});
